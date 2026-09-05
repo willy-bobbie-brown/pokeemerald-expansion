348c0833c1f1f30ac4e8029093d4a9e75d53ccd9
willy-bobbie-brown <gaytor88@gmail.com>
2026-09-06 08:21:47 +0930

    Added mirror herb to daycare center

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -51,7 +51,7 @@
 
 #else
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
+#define FLAG_ITEM_DAYCARE_MIRROR_HERB    0x20 // Mirror Herb in Daycare
 #define FLAG_UNUSED_0x021    0x21 // Unused Flag
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
