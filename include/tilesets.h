2047c6e2e52467ffe07bc5f7537c358a0dd5a338
willy-bobbie-brown <gaytor88@gmail.com>
2026-09-25 18:17:39 +1000

    BDome added to Lavaridge
    Need to change rival script I believe still, but other than that this works with the door

--- include/tilesets.h
+++ include/tilesets.h
@@ -60,5 +60,6 @@ extern const struct Tileset gTileset_SilphCo;
 extern const struct Tileset gTileset_SSAnne;
 extern const struct Tileset gTileset_SeaCottage;
 extern const struct Tileset gTileset_TrainerTower;
+extern const struct Tileset gTileset_Lavaridge;
 
 #endif //GUARD_tilesets_H
